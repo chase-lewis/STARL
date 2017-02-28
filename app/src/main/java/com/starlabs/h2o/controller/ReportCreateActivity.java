7109d2f63d64d3e99899df881fc0cff1a608e732
KavinKrishnan <kavinkrishnan@gmail.com>
2017-02-27 19:36:45 -0500

    created report view and working on activity java file

---
+++ app/src/main/java/com/starlabs/h2o/controller/ReportCreateActivity.java
@@ -0,0 +1,118 @@
+package com.starlabs.h2o.controller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.starlabs.h2o.R;
+import com.starlabs.h2o.model.Person;
+
+/**
+ * Activity to edit the user profile
+ *
+ * @author tejun
+ */
+public class ReportCreateActivity extends AppCompatActivity {
+
+    // Intent message ids
+    public static final String TO_MAIN = "TO_MAIN";
+    public static final String PROF_UPDATE = "PROF UPDATE";
+
+    // Field views
+    private TextView reportDateText;
+    private TextView reportNumText;
+    private TextView reportLocField;
+    private TextView reportReporterNmae;
+    private Spinner waterType;
+    private Spinner waterCond;
+
+    // User passed into this activity
+    private Person user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reportcreate);
+
+        // Set up the fields for the user profile
+        reportDateText = (TextView) findViewById(R.id.reportDate);;
+        reportNumText = (TextView) findViewById(R.id.reportNum);;
+        reportLocField = (TextView) findViewById(R.id.reportLocation);;
+        reportReporterNmae = (TextView) findViewById(R.id.reportReportName);;
+        waterType = (Spinner) findViewById(R.id.WaterTypeSpin);;
+        waterCond = (Spinner) findViewById(R.id.WaterCondSpin);;
+
+//        // Get the user from the intent message
+//        user = getIntent().getParcelableExtra(RegisterActivity.REG_INTENT);
+//
+//        // Set up the text pre-defined values
+//        nameField.setText(user.getName());
+//        emailField.setText(user.getEmail());
+//        addressField.setText(user.getAddress());
+//
+//        // Done button setup
+//        Button profileDoneButton = (Button) findViewById(R.id.profile_done_button);
+//        profileDoneButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                onProfileDonePressed(view);
+//            }
+//        });
+//
+//        // Cancel button setup
+//        Button profileCancelButton = (Button) findViewById(R.id.profile_cancel_button);
+//        profileCancelButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                onCancelPressed(view);
+//            }
+//        });
+    }
+
+    /**
+     * Method to exit the activity to main.
+     *
+     * @param view the parameter View
+     */
+    protected void onProfileDonePressed(View view) {
+//        // Update the user model from the fields
+//        user.setName(nameField.getText().toString());
+//        user.setEmail(emailField.getText().toString());
+//        user.setAddress(addressField.getText().toString());
+//
+//        // Store the user in firebase, overwriting any values already there
+//        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+//        mDatabase.child("users").child(user.getUsername()).setValue(user);
+//
+//        // Transition to the proper activity
+//        if (getIntent().getBooleanExtra(TO_MAIN, false)) {
+//            Intent toMain = new Intent(ProfileActivity.this, MainActivity.class);
+//            toMain.putExtra(MainActivity.USER_TO_MAIN, user);
+//            startActivity(toMain);
+//        } else {
+//            // TODO
+//            Intent result = new Intent();
+//            result.putExtra(PROF_UPDATE, user);
+//            setResult(Activity.RESULT_OK, result);
+//        }
+//        finish();
+    }
+
+    /**
+     * Method to exit the activity back to caller.
+     *
+     * @param view the parameter View
+     */
+    protected void onCancelPressed(View view) {
+        finish();
+    }
+
+}
+
+
