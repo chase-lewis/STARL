1c239353c4300943378c200b66fee0373c2daf20
Chase Lewis <chaselewis@gatech.edu>
2017-03-01 17:27:20 -0500

    Added firebase to the ViewReportsActivity

--- app/src/main/java/com/starlabs/h2o/controller/ViewReportsActivity.java
+++ app/src/main/java/com/starlabs/h2o/controller/ViewReportsActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.starlabs.h2o.R;
 import com.starlabs.h2o.model.WaterReport;
 
@@ -13,6 +18,7 @@ import java.util.List;
 public class ViewReportsActivity extends AppCompatActivity {
     private RecyclerView mRecycler;
     private RVAdapter adapter;
+    private List<WaterReport> waterReports;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +27,22 @@ public class ViewReportsActivity extends AppCompatActivity {
         mRecycler = (RecyclerView) findViewById(R.id.recycler_view);
         mRecycler.setLayoutManager(new LinearLayoutManager(this));
 
-        List<WaterReport> waterReports;
         //TODO: Obtain list of Water Reports and put it in waterReports
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("waterReports").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int numReports = 0;
+                for (DataSnapshot report : dataSnapshot.getChildren()) {
+                    waterReports.add(report.getValue(WaterReport.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Do nothing
+            }
+        });
 
         //adapter = new RVAdapter(waterReports);
         mRecycler.setAdapter(adapter);
