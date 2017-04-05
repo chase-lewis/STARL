6478baf76771f5581f0e367e3c69a35e04eae7c4
KavinKrishnan <kavinkrishnan@gmail.com>
2017-04-05 01:37:12 -0400

    included unnec. imports

--- app/src/test/java/com/starlabs/h2o/controller/HomeActivityTest.java
+++ app/src/test/java/com/starlabs/h2o/controller/HomeActivityTest.java
@@ -1,43 +1,16 @@
 package com.starlabs.h2o.controller;
 
 import android.app.Fragment;
-
 import org.junit.Test;
 import com.starlabs.h2o.R;
-
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-
-import com.google.android.gms.maps.MapFragment;
 import com.starlabs.h2o.controller.purity_report.CreatePurityReportFragment;
 import com.starlabs.h2o.controller.purity_report.ViewPurityReportsFragment;
 import com.starlabs.h2o.controller.report.SetupHistogramFragment;
-import com.starlabs.h2o.controller.report.ViewHistogramFragment;
 import com.starlabs.h2o.controller.report.ViewMapFragment;
 import com.starlabs.h2o.controller.user.ViewUserProfileFragment;
 import com.starlabs.h2o.controller.water_report.CreateWaterReportFragment;
 import com.starlabs.h2o.controller.water_report.ViewWaterReportsFragment;
-import com.starlabs.h2o.dao.ContentProvider;
-import com.starlabs.h2o.dao.ContentProviderFactory;
-import com.starlabs.h2o.model.report.WaterReport;
-import com.starlabs.h2o.model.user.User;
-import com.starlabs.h2o.model.user.UserType;
 
 /**
  * Created by kavin on 4/5/2017.
